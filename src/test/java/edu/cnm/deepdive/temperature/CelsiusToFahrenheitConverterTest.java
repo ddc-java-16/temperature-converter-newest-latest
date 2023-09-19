7fb8f669da0ad3c72905f99bf3d0bea71a1d582c
che money <cmelendez15@cnm.edu>
2023-09-19 11:40:03 -0500

    init commit

---
+++ src/test/java/edu/cnm/deepdive/temperature/CelsiusToFahrenheitConverterTest.java
@@ -0,0 +1,24 @@
+package edu.cnm.deepdive.temperature;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CelsiusToFahrenheitConverterTest {
+
+  @Test
+  void convert() {
+    assertEquals(
+        32,
+        new CelsiusToFahrenheitConverter().convert(0),
+        0.0001);
+    assertEquals(
+        212,
+        new CelsiusToFahrenheitConverter().convert(100),
+        0.0001);
+    assertEquals(
+        -40,
+        new CelsiusToFahrenheitConverter().convert(-40),
+        0.0001);
+  }
+}
